77243d8904ca51fdd04bb6f8d378991ca4d0cbab
heolyun <hl620@naver.com>
2025-10-29 14:33:28 +0900

    자바 시작, 변수 기본 개념, gitignore 추가

---
+++ src/be_study/start/HelloWorld.java
@@ -0,0 +1,26 @@
+package be_study.start;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		System.out.println("Hello, World!");
+		
+		// 자바 코드 실행 : ctrl + F11
+		//한줄주석
+		/*여러줄
+		 * 주석
+		 * 엔터를
+		 * 쳐도
+		 * 주석
+		 */
+		
+		//주석 똑같이 ctrl + / 가능 
+		//         ctrl + Shift + c 도 가능
+		
+		// sysout 입력후 ,ctrl + Space 누르면 자동완성
+		System.out.println("자동완성");
+	}
+
+}
